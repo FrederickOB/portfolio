fac6c77b734be669239f1c57b85b88ddd9ff42de
Frederick Ofori-Boadu <friedosei@gmail.com>
2024-02-22 14:03:36 +0000

    added umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,11 @@ export default function RootLayout({
           GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}
         />
       )}
+      <script
+        defer
+        src="https://analytics.us.umami.is/script.js"
+        data-website-id="dfbb00ae-1979-4d7d-acdb-5598e2ed0dec"
+      ></script>
       <body className="bg-white dark:bg-black">{children}</body>
     </html>
   );
